c42f40091ef4406138524b31b63e593dc3d43480
xiongyicheng <369874203@qq.com>
2016-09-05 21:20:15 +0800

    up

--- index.js
+++ index.js
@@ -1,68 +1,7 @@
-// var a = 5;
-// var b = 6;
-// var c = '5';
-// document.write(a+b+'<br/>');
-// document.write(a+c+'<br/>');
-// document.write(a-b+'<br/>');
-// document.write(a-c+'<br/>');
-//
-// document.write(a>b);
-// document.write('<br/>');
-// document.write(a<b || b<c);
-// document.write('<br/>');
-// document.write(a<b && b<c);
-// document.write('<br/>');
-// document.write(a===c);
-// document.write('<br/>');
-// document.write(a>b?'aaa':'bbb');
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.write('hello world.');
+    res.end();
+});
 
-// for(var i=0;i<10;i++) {
-//   console.log(i);
-// }
-
-// switch (a) {
-//   case 5:
-//     console.log(555);
-//     break;
-//   case 6:
-//      console.log(777);
-//     break;
-//   default:
-//     console.log(666);
-//     break;
-// }
-
-// var fun1 = function(...args) {
-//   console.log(args);
-// }
-// fun1(1,1,1,1,1);
-
-// function fun2(a,b='123') {
-//   console.log(a,b);
-// }
-// fun2('sss');
-
-// function Cat(name) {
-//   this.name = name;
-// }
-//
-// var cat = new Cat('123ss');
-// console.log(cat.name);
-
-// function fun3() {
-//   return 4;
-// }
-//
-// alert(fun3);
-
-// var obj = {
-//   name:function(a) {
-//     return a;
-//   }
-// }
-//
-// console.log(obj.name(55));
-
-// var c = 'asdsdad';
-// document.write(c);
-// document.write(c.big());
+server.listen(3000);
