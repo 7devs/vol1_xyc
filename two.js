1968eb68918548317b6714d829a6e352a9a97163
xiongyicheng <369874203@qq.com>
2016-09-05 19:02:05 +0800

    up

--- two.js
+++ two.js
@@ -9,5 +9,15 @@ document.write('<br/>');
 
 var arr = [1,2,3,'ddd','444ff'];
 var arrB = ['rr','ttt'];
-console.log(arr.concat(arr,arrB,['fd3333',8888]));
-document.write(arr.join('-'));
+// console.log(arr.concat(arr,arrB,['fd3333',8888]));
+// document.write(arr.join('-'));
+//
+// console.log(arr.pop());
+// console.log(arr);
+//
+// console.log(arr.reverse());
+ console.log(arr);
+// console.log(arr.slice(1,-2));
+
+console.log(arr.splice(-2,1));
+console.log(arr);
