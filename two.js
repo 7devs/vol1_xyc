569e827798efee86cdf69fa8fd9b788f30ef32ed
xiongyicheng <369874203@qq.com>
2016-09-05 18:42:25 +0800

    up

---
+++ two.js
@@ -0,0 +1,13 @@
+document.write(Math.abs(-111));  //111    绝对值
+document.write('<br/>');
+document.write(Math.max(22,11));
+document.write('<br/>');
+
+
+
+
+
+var arr = [1,2,3,'ddd','444ff'];
+var arrB = ['rr','ttt'];
+console.log(arr.concat(arr,arrB,['fd3333',8888]));
+document.write(arr.join('-'));
